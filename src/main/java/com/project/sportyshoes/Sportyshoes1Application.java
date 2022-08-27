2d074525b5f0f3b5a67aadb4480e72a4ae1acf66
pratyaksh0615 <pratyaksh.saxena151298@gmail.com>
2022-08-27 23:05:28 +0530

    Init

---
+++ src/main/java/com/project/sportyshoes/Sportyshoes1Application.java
@@ -0,0 +1,13 @@
+package com.project.sportyshoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sportyshoes1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sportyshoes1Application.class, args);
+	}
+
+}
